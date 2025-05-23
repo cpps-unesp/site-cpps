b4e6caf8c4dc3b8c93b4b24650b60b3f0ca6bb34
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-05-23 11:15:29 -0300

    base do astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
    site: 'https://site-cpps.github.io',
-  base: '/',
+  base: 'site-cpps',
   integrations: [ starlight({ title: 'CPPS/UNESP' })]
 });
