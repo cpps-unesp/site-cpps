7294f308cfbd272c295f305e865a303758e75fb8
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-05-24 23:20:38 -0300

    estrutura inicial com starlight e daisyui 2

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,24 @@ import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+import slugify from "slugify"; // Certifique-se de ter instalado: npm install slugify
+
+// Função auxiliar para gerar slug
+function createSlug(date: string, num: string): string {
+  
+  return slugify(`${date}-${num}`, { lower: true, strict: true });
+}
+
 const noticias = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.string(),
     resumo: z.string(),
     image: z.string(),
-  }),
+  }).transform((data) => ({
+    ...data,
+    slug: createSlug(data.date,data.title),
+  })),
 });
 
 
--- src/content/noticias/nome-da-noticia.md
+++ src/content/noticias/nome-da-noticia.md
@@ -2,7 +2,7 @@
 title: "Nova parceria internacional"
 date: "2024-07-01"
 resumo: "Firmamos uma parceria estratégica com uma universidade europeia."
-image: "/images/noticias/parceria.jpg"
+image: "/imagens/equipe/ligia_simplicio.jpeg"
 ---
 
 O CPPS estabeleceu uma nova parceria com a Universidade de Lisboa, visando ampliar projetos conjuntos...
