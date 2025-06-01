14350aa29b8968e009b431bd53f21276f1aca090
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-06-01 20:25:47 -0300

    ajustes astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: "https://cpps-unesp.github.io",
-  base: "/",
+  base: "/site-cpps",
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
