8c92f2358634446cd76d2a8b790f4911edc2ebb0
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-06-17 21:50:50 -0300

    estruturação inicial de iniciativas

--- src/i18n/routes.ts
+++ src/i18n/routes.ts
@@ -4,10 +4,47 @@ export const routeTranslations = {
     en: "initiatives/projects",
     es: "iniciativas/proyectos",
   },
+
+  "iniciativas/projetos-de-pesquisa": {
+    pt: "iniciativas/projetos-de-pesquisa",
+    en: "initiatives/research-projects",
+    es: "iniciativas/proyectos-de-investigacion",
+  },
+  "iniciativas/projetos-de-dados": {
+    pt: "iniciativas/projetos-de-dados",
+    en: "initiatives/data-projects",
+    es: "iniciativas/proyectos-de-datos",
+  },
+
   "iniciativas/cafe-com-ciencia": {
     pt: "iniciativas/cafe-com-ciencia",
     en: "initiatives/coffee-with-science",
     es: "iniciativas/cafe-con-ciencia",
+  },
+  "iniciativas/publicacoes": {
+    pt: "iniciativas/publicacoes",
+    en: "initiatives/publications",
+    es: "iniciativas/publicaciones",
+  },
+  "iniciativas/material-de-apoio": {
+    pt: "iniciativas/material-de-apoio",
+    en: "initiatives/support-material",
+    es: "iniciativas/material-de-apoyo",
+  },
+  "iniciativas/oficinas": {
+    pt: "iniciativas/oficinas",
+    en: "initiatives/workshops",
+    es: "iniciativas/talleres",
+  },
+  "iniciativas/solucoes-tecnologicas": {
+    pt: "iniciativas/solucoes-tecnologicas",
+    en: "initiatives/technological-solutions",
+    es: "iniciativas/soluciones-tecnologicas",
+  },
+  "iniciativas/parcerias": {
+    pt: "iniciativas/parcerias",
+    en: "initiatives/partnerships",
+    es: "iniciativas/alianzas",
   },
   "institucional/sobre": {
     pt: "institucional/sobre",
@@ -30,5 +67,3 @@ export const routeTranslations = {
     es: "noticias",
   },
 } as const;
-
-
