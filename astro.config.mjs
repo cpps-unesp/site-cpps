de7fbfe545ee1c73558c1f2001a301cfc6049e8b
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-12-09 13:35:37 -0300

    estrutura de docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import pagefind from "astro-pagefind";
 import starlight from "@astrojs/starlight";
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://cpps-unesp.github.io",
   output: 'static',
@@ -13,7 +12,91 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [starlight({
+  integrations: [
+    starlight({
       title: 'Documentação CPPS',
-    }), pagefind(), mdx()],
+      sidebar: [
+        {
+          label: "Documentação",
+          items: [
+            {
+              label: "Introdução",
+              slug: "docs", // src/content/docs/docs/index.mdx
+            },
+          ],
+        },
+
+        {
+          label: "Projetos",
+          items: [
+            // --- DADOS ---
+            {
+              label: "Dados",
+              items: [
+                {
+                  label: "Visão geral",
+                  slug: "docs/projetos/dados",   // src/content/docs/projetos/dados/index.mdx
+                },
+
+                // --------- HEMEROTECA PEB ----------
+                {
+                  label: "Hemeroteca PEB",
+                  items: [
+                    {
+                      label: "Apresentação",
+                      slug: "docs/projetos/dados/hemeroteca-peb",
+                    },
+                    //depois adiciona:
+                    // { label: "Equipe", slug: "docs/projetos/dados/hemeroteca-peb/equipe" },
+                    // { label: "Atividades Realizadas", slug: "docs/projetos/dados/hemeroteca-peb/atividades-realizadas" },
+                    // etc.
+                  ],
+                },
+
+                {
+                  label: "ThesisBR",
+                  items: [
+            
+                    {
+                      label: "Conheça o projeto",
+                      link: "https://cpps-unesp.github.io/thesisbr/", //URL real aqui
+                    },
+                  ],
+                },
+              ],
+            },
+
+            // --- ENSINO ---
+            {
+              label: "Ensino",
+              items: [
+                {
+                  label: "Visão geral",
+                  slug: "docs/projetos/ensino",
+                },
+                {
+                  label: "Trilha de Dados",
+                  slug: "docs/projetos/ensino/trilha-de-dados",
+                },
+              ],
+            },
+
+            // --- SISTEMAS ---
+            {
+              label: "Sistemas",
+              autogenerate: { directory: "docs/projetos/sistemas" },
+            },
+
+            // --- EXTENSÃO ---
+            {
+              label: "Extensão",
+              autogenerate: { directory: "docs/projetos/extensao" },
+            },
+          ],
+        },
+      ],
+    }),
+    pagefind(),
+    mdx(),
+  ],
 });
