c5b5f539e27758aff4f6f736e4832845a3989e05
Rafael de Almeida <ra.almeida@unesp.br>
2026-02-03 20:08:41 -0300

    Revert "Adiciona middleware de detecção de idioma com Cloudflare adapter (#49)"
    This reverts commit cf6ec5e2aeb20b8804e35d876d9249246cc052e1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import pagefind from "astro-pagefind";
 import starlight from "@astrojs/starlight";
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cpps.franca.unesp.br",
-  output: 'server',
-  adapter: cloudflare(),
+  site: "https://cpps-unesp.github.io",
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [starlight({
       title: 'Documentação CPPS',
-      disable404Route: true,
     }), pagefind(), mdx()],
 });
