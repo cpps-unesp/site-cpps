d69c4f347e186efae899980bfe562d9a350268b4
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2026-02-10 19:22:49 -0300

    chore: padroniza configuração Cloudflare para Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import pagefind from "astro-pagefind";
+import tailwindcss from '@tailwindcss/vite';
+import pagefind from 'astro-pagefind';
 import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cpps.franca.unesp.br",
+  site: 'https://cpps.franca.unesp.br',
   output: 'server',
   adapter: cloudflare({
-    imageService: 'passthrough',
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: false,
+    },
   }),
   vite: {
     plugins: [tailwindcss()],
