60ca4cfa7b9c57fb02713ac141281a9802bd57e8
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2026-02-10 19:45:05 -0300

    chore: remove código não utilizado para limpar warnings do CI

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('verify news on home page', async ({ page }) => {
   await page.goto('http://localhost:4321/pt');
