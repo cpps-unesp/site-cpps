59826d7a59a22eaf7db788b44d0dbb453719d99a
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2026-05-11 09:17:40 -0300

    fix(types): suprime no-empty-object-type no App.Locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,5 +3,6 @@
 type Runtime = import('@astrojs/cloudflare').Runtime;
 
 declare namespace App {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Locals extends Runtime {}
 }
